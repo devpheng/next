343d1f1fad1ea89958da67bf727ca596de12d7e7
pheng <pheng@marinax.co.jp>
2024-01-04 15:42:14 +0700

    Add layout header footer

---
+++ components/script.tsx
@@ -0,0 +1,14 @@
+export const Script = () => {
+  return (
+    <>
+        <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="/assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="/assets/vendor/nouislider/nouislider.min.js"></script>
+        <script src="/assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="/assets/vendor/choices.js/public/assets/scripts/choices.min.js"></script>
+        <script src="/assets/js/front.js"></script>
+        <script src="/assets/js/injectsvg.js"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"></link>
+    </>
+  )
+}
