2d7bb4efaecaa39af4baf693d94eb11fd62bdef0
pheng <pheng@marinax.co.jp>
2024-01-05 09:57:14 +0700

    Create products componenct

--- app/page.tsx
+++ app/page.tsx
@@ -1,77 +1,86 @@
 import { Categories } from '@/components/categories'
 import { Hero } from '@/components/hero'
+import { Products } from '@/components/products'
 import Image from 'next/image'
 // import styles from './page.module.css'
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Hero />
-      <Categories />
-        <section className="py-5 bg-light">
-          <div className="container">
-            <div className="row text-center gy-3">
-              <div className="col-lg-4">
-                <div className="d-inline-block">
-                  <div className="d-flex align-items-end">
-                    <svg className="svg-icon svg-icon-big svg-icon-light">
-                      {/* <use xlink:href="#delivery-time-1"> </use> */}
-                    </svg>
-                    <div className="text-start ms-3">
-                      <h6 className="text-uppercase mb-1">Free shipping</h6>
-                      <p className="text-sm mb-0 text-muted">Free shipping worldwide</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="d-inline-block">
-                  <div className="d-flex align-items-end">
-                    <svg className="svg-icon svg-icon-big svg-icon-light">
-                      {/* <use xlink:href="#helpline-24h-1"> </use> */}
-                    </svg>
-                    <div className="text-start ms-3">
-                      <h6 className="text-uppercase mb-1">24 x 7 service</h6>
-                      <p className="text-sm mb-0 text-muted">Free shipping worldwide</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="d-inline-block">
-                  <div className="d-flex align-items-end">
-                    <svg className="svg-icon svg-icon-big svg-icon-light">
-                      {/* <use xlink:href="#label-tag-1"> </use> */}
-                    </svg>
-                    <div className="text-start ms-3">
-                      <h6 className="text-uppercase mb-1">Festivaloffers</h6>
-                      <p className="text-sm mb-0 text-muted">Free shipping worldwide</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+    return (
+        <div className="container">
+            <Hero />
+            <Categories />
+            <section className="py-5">
+                <header>
+                    <p className="small text-muted small text-uppercase mb-1">Made the hard way</p>
+                    <h2 className="h5 text-uppercase mb-4">Top trending products</h2>
+                </header>
+                <Products />
+            </section>
 
-        <section className="py-5">
-          <div className="container p-0">
-            <div className="row gy-3">
-              <div className="col-lg-6">
-                <h5 className="text-uppercase">Let's be friends!</h5>
-                <p className="text-sm text-muted mb-0">Nisi nisi tempor consequat laboris nisi.</p>
-              </div>
-              <div className="col-lg-6">
-                <form action="#">
-                  <div className="input-group">
-                    <input className="form-control form-control-lg" type="email" placeholder="Enter your email address" aria-describedby="button-addon2" />
-                    <button className="btn btn-dark" id="button-addon2" type="submit">Subscribe</button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </section>
-    </div>
-  )
+            <section className="py-5 bg-light">
+                <div className="container">
+                    <div className="row text-center gy-3">
+                        <div className="col-lg-4">
+                            <div className="d-inline-block">
+                                <div className="d-flex align-items-end">
+                                    <svg className="svg-icon svg-icon-big svg-icon-light">
+                                        {/* <use xlink:href="#delivery-time-1"> </use> */}
+                                    </svg>
+                                    <div className="text-start ms-3">
+                                        <h6 className="text-uppercase mb-1">Free shipping</h6>
+                                        <p className="text-sm mb-0 text-muted">Free shipping worldwide</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4">
+                            <div className="d-inline-block">
+                                <div className="d-flex align-items-end">
+                                    <svg className="svg-icon svg-icon-big svg-icon-light">
+                                        {/* <use xlink:href="#helpline-24h-1"> </use> */}
+                                    </svg>
+                                    <div className="text-start ms-3">
+                                        <h6 className="text-uppercase mb-1">24 x 7 service</h6>
+                                        <p className="text-sm mb-0 text-muted">Free shipping worldwide</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4">
+                            <div className="d-inline-block">
+                                <div className="d-flex align-items-end">
+                                    <svg className="svg-icon svg-icon-big svg-icon-light">
+                                        {/* <use xlink:href="#label-tag-1"> </use> */}
+                                    </svg>
+                                    <div className="text-start ms-3">
+                                        <h6 className="text-uppercase mb-1">Festivaloffers</h6>
+                                        <p className="text-sm mb-0 text-muted">Free shipping worldwide</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="py-5">
+                <div className="container p-0">
+                    <div className="row gy-3">
+                        <div className="col-lg-6">
+                            <h5 className="text-uppercase">Let's be friends!</h5>
+                            <p className="text-sm text-muted mb-0">Nisi nisi tempor consequat laboris nisi.</p>
+                        </div>
+                        <div className="col-lg-6">
+                            <form action="#">
+                                <div className="input-group">
+                                    <input className="form-control form-control-lg" type="email" placeholder="Enter your email address" aria-describedby="button-addon2" />
+                                    <button className="btn btn-dark" id="button-addon2" type="submit">Subscribe</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
 }
---
+++ components/products.tsx
@@ -0,0 +1,135 @@
+export const Products = () => {
+    return (
+        <div className="row">
+            <div className="col-xl-3 col-lg-4 col-sm-6">
+                <div className="product text-center">
+                    <div className="position-relative mb-3">
+                        <div className="badge text-white bg-"></div>
+                        <a className="d-block" href="detail.html"><img className="img-fluid w-100" src="/assets/img/product-1.jpg" alt="..." /></a>
+                        <div className="product-overlay">
+                            <ul className="mb-0 list-inline">
+                                <li className="list-inline-item m-0 p-0"><a className="btn btn-sm btn-outline-dark" href="#!"><i className="far fa-heart"></i></a></li>
+                                <li className="list-inline-item m-0 p-0"><a className="btn btn-sm btn-dark" href="cart.html">Add to cart</a></li>
+                                <li className="list-inline-item me-0"><a className="btn btn-sm btn-outline-dark" href="#productView" data-bs-toggle="modal"><i className="fas fa-expand"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <h6> <a className="reset-anchor" href="detail.html">Kui Ye Chen’s AirPods</a></h6>
+                    <p className="small text-muted">$250</p>
+                </div>
+            </div>
+            <div className="col-xl-3 col-lg-4 col-sm-6">
+                <div className="product text-center">
+                    <div className="position-relative mb-3">
+                        <div className="badge text-white bg-primary">Sale</div><a className="d-block" href="detail.html"><img className="img-fluid w-100" src="/assets/img/product-2.jpg" alt="..." /></a>
+                        <div className="product-overlay">
+                            <ul className="mb-0 list-inline">
+                                <li className="list-inline-item m-0 p-0"><a className="btn btn-sm btn-outline-dark" href="#!"><i className="far fa-heart"></i></a></li>
+                                <li className="list-inline-item m-0 p-0"><a className="btn btn-sm btn-dark" href="cart.html">Add to cart</a></li>
+                                <li className="list-inline-item me-0"><a className="btn btn-sm btn-outline-dark" href="#productView" data-bs-toggle="modal"><i className="fas fa-expand"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <h6> <a className="reset-anchor" href="detail.html">Air Jordan 12 gym red</a></h6>
+                    <p className="small text-muted">$300</p>
+                </div>
+            </div>
+            <div className="col-xl-3 col-lg-4 col-sm-6">
+                <div className="product text-center">
+                    <div className="position-relative mb-3">
+                        <div className="badge text-white bg-"></div><a className="d-block" href="detail.html"><img className="img-fluid w-100" src="/assets/img/product-3.jpg" alt="..." /></a>
+                        <div className="product-overlay">
+                            <ul className="mb-0 list-inline">
+                                <li className="list-inline-item m-0 p-0"><a className="btn btn-sm btn-outline-dark" href="#!"><i className="far fa-heart"></i></a></li>
+                                <li className="list-inline-item m-0 p-0"><a className="btn btn-sm btn-dark" href="cart.html">Add to cart</a></li>
+                                <li className="list-inline-item me-0"><a className="btn btn-sm btn-outline-dark" href="#productView" data-bs-toggle="modal"><i className="fas fa-expand"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <h6> <a className="reset-anchor" href="detail.html">Cyan cotton t-shirt</a></h6>
+                    <p className="small text-muted">$25</p>
+                </div>
+            </div>
+            <div className="col-xl-3 col-lg-4 col-sm-6">
+                <div className="product text-center">
+                    <div className="position-relative mb-3">
+                        <div className="badge text-white bg-info">New</div><a className="d-block" href="detail.html"><img className="img-fluid w-100" src="/assets/img/product-4.jpg" alt="..." /></a>
+                        <div className="product-overlay">
+                            <ul className="mb-0 list-inline">
+                                <li className="list-inline-item m-0 p-0"><a className="btn btn-sm btn-outline-dark" href="#!"><i className="far fa-heart"></i></a></li>
+                                <li className="list-inline-item m-0 p-0"><a className="btn btn-sm btn-dark" href="cart.html">Add to cart</a></li>
+                                <li className="list-inline-item me-0"><a className="btn btn-sm btn-outline-dark" href="#productView" data-bs-toggle="modal"><i className="fas fa-expand"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <h6> <a className="reset-anchor" href="detail.html">Timex Unisex Originals</a></h6>
+                    <p className="small text-muted">$351</p>
+                </div>
+            </div>
+            <div className="col-xl-3 col-lg-4 col-sm-6">
+                <div className="product text-center">
+                    <div className="position-relative mb-3">
+                        <div className="badge text-white bg-danger">Sold</div><a className="d-block" href="detail.html"><img className="img-fluid w-100" src="/assets/img/product-5.jpg" alt="..."/></a>
+                        <div className="product-overlay">
+                            <ul className="mb-0 list-inline">
+                                <li className="list-inline-item m-0 p-0"><a className="btn btn-sm btn-outline-dark" href="#!"><i className="far fa-heart"></i></a></li>
+                                <li className="list-inline-item m-0 p-0"><a className="btn btn-sm btn-dark" href="cart.html">Add to cart</a></li>
+                                <li className="list-inline-item me-0"><a className="btn btn-sm btn-outline-dark" href="#productView" data-bs-toggle="modal"><i className="fas fa-expand"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <h6> <a className="reset-anchor" href="detail.html">Red digital smartwatch</a></h6>
+                    <p className="small text-muted">$250</p>
+                </div>
+            </div>
+            <div className="col-xl-3 col-lg-4 col-sm-6">
+                <div className="product text-center">
+                    <div className="position-relative mb-3">
+                        <div className="badge text-white bg-"></div><a className="d-block" href="detail.html"><img className="img-fluid w-100" src="/assets/img/product-6.jpg" alt="..." /></a>
+                        <div className="product-overlay">
+                            <ul className="mb-0 list-inline">
+                                <li className="list-inline-item m-0 p-0"><a className="btn btn-sm btn-outline-dark" href="#!"><i className="far fa-heart"></i></a></li>
+                                <li className="list-inline-item m-0 p-0"><a className="btn btn-sm btn-dark" href="cart.html">Add to cart</a></li>
+                                <li className="list-inline-item me-0"><a className="btn btn-sm btn-outline-dark" href="#productView" data-bs-toggle="modal"><i className="fas fa-expand"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <h6> <a className="reset-anchor" href="detail.html">Nike air max 95</a></h6>
+                    <p className="small text-muted">$300</p>
+                </div>
+            </div>
+            <div className="col-xl-3 col-lg-4 col-sm-6">
+                <div className="product text-center">
+                    <div className="position-relative mb-3">
+                        <div className="badge text-white bg-"></div><a className="d-block" href="detail.html"><img className="img-fluid w-100" src="/assets/img/product-7.jpg" alt="..." /></a>
+                        <div className="product-overlay">
+                            <ul className="mb-0 list-inline">
+                                <li className="list-inline-item m-0 p-0"><a className="btn btn-sm btn-outline-dark" href="#!"><i className="far fa-heart"></i></a></li>
+                                <li className="list-inline-item m-0 p-0"><a className="btn btn-sm btn-dark" href="cart.html">Add to cart</a></li>
+                                <li className="list-inline-item me-0"><a className="btn btn-sm btn-outline-dark" href="#productView" data-bs-toggle="modal"><i className="fas fa-expand"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <h6> <a className="reset-anchor" href="detail.html">Joemalone Women prefume</a></h6>
+                    <p className="small text-muted">$25</p>
+                </div>
+            </div>
+            <div className="col-xl-3 col-lg-4 col-sm-6">
+                <div className="product text-center">
+                    <div className="position-relative mb-3">
+                        <div className="badge text-white bg-"></div><a className="d-block" href="detail.html"><img className="img-fluid w-100" src="/assets/img/product-8.jpg" alt="..." /></a>
+                        <div className="product-overlay">
+                            <ul className="mb-0 list-inline">
+                                <li className="list-inline-item m-0 p-0"><a className="btn btn-sm btn-outline-dark" href="#!"><i className="far fa-heart"></i></a></li>
+                                <li className="list-inline-item m-0 p-0"><a className="btn btn-sm btn-dark" href="cart.html">Add to cart</a></li>
+                                <li className="list-inline-item me-0"><a className="btn btn-sm btn-outline-dark" href="#productView" data-bs-toggle="modal"><i className="fas fa-expand"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <h6> <a className="reset-anchor" href="detail.html">Apple Watch</a></h6>
+                    <p className="small text-muted">$351</p>
+                </div>
+            </div>
+        </div>
+    )
+}
